9d67d18141592ad5b3dc6bea93b1f92693770b84
Alex Kosyuk <kosyuk@gmail.com>
2018-04-02 22:32:24 +0300

    initial project

---
+++ main.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
+const port = process.argv[2] || 9000;
+
+http.createServer(function (req, res) {
+  console.log(`${req.method} ${req.url}`);
+  
+  // parse URL
+  const parsedUrl = url.parse(req.url);
+  let pathname = `.${parsedUrl.pathname}`;
+
+  fs.exists(pathname, function (exist) {
+    if(!exist) {
+      // if the file is not found, return 404
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+      return;
+    }
+
+    // read file from file system
+    fs.readFile(pathname, function(err, data){
+      if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+      } else {
+        // if the file is found, set Content-type and send data
+        res.setHeader('Content-type', 'application/json' );
+        res.end(data);
+      }
+    });
+  });
+
+
+}).listen(parseInt(port));
+
+console.log(`Server listening on port ${port}`);
---
+++ readme.md
@@ -0,0 +1,8 @@
+1. Start application
+```
+npm run start
+```
+2. Open in browser 
+```
+http://localhost:9000/json/sample.json
+```
